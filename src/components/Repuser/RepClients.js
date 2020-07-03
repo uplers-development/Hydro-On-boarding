d1e3981f20d9c9418d4af5c64d14490972c4e0bb
webuplers <web@uplers.com>
2020-07-03 10:32:36 +0530

    css files changes

--- src/components/Repuser/RepClients.js
+++ src/components/Repuser/RepClients.js
@@ -83,6 +83,56 @@ class RepClients extends React.Component {
 					{/*<!--Clients main blok start-->*/}
 					<div className="d-flex flex-wrap clients-main">
 					
+					{/*<!--Top filter block Start-->*/}
+					<div className="fileter-block d-flex flex-wrap">
+						
+							{/*<!--Select box start-->*/}
+							<div className="select-box">
+								<span>Bulk Action</span>
+								<ul className="list">
+									<li><a href="#" title="Bulk Action 1">Bulk Action 1</a></li>
+									<li><a href="#" title="Bulk Action 2">Bulk Action 2</a></li>
+									<li><a href="#" title="Bulk Action 3">Bulk Action 3</a></li>
+									
+								</ul>
+							</div>
+							<div className="btn-block">
+								<button className="common-btn-blue"><span>APPLY</span></button>
+							</div>
+							{/*<!--Select box end-->*/}	
+							
+							{/*<!--Right search and sort block start-->*/}
+							<div className="search-sort-block d-flex flex-wrap align-center">
+
+								{/*<!--Auto search box start-->*/}
+								<div className="auto-search-box">
+									<form>
+										<div className="autocomplete-ss">
+											<input id="myInput" type="text" name="hydro" />
+										</div>
+									</form>
+								</div>
+								{/*<!--Auto search box end-->*/}
+
+
+								{/*<!--Sort by start-->*/}
+								<div className="d-flex flex-wrap sort-by">
+									<div className="sort-selected d-flex flex-wrap align-center">
+										<h2>Sort by</h2>
+									</div>
+									<div className="drop-down-menu">
+										<ul>
+											<li><a href="#" title="Purchase date newest">Purchase date newest</a></li>
+											<li><a href="#" title="Purchase date oldest">Purchase date oldest</a></li>
+											<li><a href="#" title="A-Z">A-Z</a></li>
+										</ul>
+									</div>
+								</div>
+								{/*<!--Sort by end-->*/}
+							</div>
+						
+					</div>
+					{/*<!--Top filter block End-->*/}
 						
 						
 					</div>

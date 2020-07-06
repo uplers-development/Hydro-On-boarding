aa7c263bbf8fa3fc122e83fe5472acef835a2ab7
webuplers <web@uplers.com>
2020-07-06 19:59:48 +0530

    Design client add page

--- src/components/Repuser/RepClients_add.js
+++ src/components/Repuser/RepClients_add.js
@@ -65,7 +65,7 @@ class RepClients_add extends React.Component {
 
 						<div className="d-flex flex-wrap user-log">
 							<div className="user-image-name d-flex flex-wrap align-center">
-								<img src={require("../../images/john-smith.png")} alt="Prfile image" />
+								<img src={require("../../images/user-scond.png")} alt="Prfile image" />
 								<h2>Username</h2>
 							</div>
 							<div className="drop-down-menu">
@@ -134,6 +134,13 @@ class RepClients_add extends React.Component {
 									 <label>Password</label>
   									 <input type="password" name="password" />
 									</div>
+<div className="send-user-notification">
+	<div class="d-flex flex-wrap notification"><h3>Send User Notifications</h3>
+	<div className="checkbox-cust"><input type="checkbox" id="checkbox5" />
+									<label for="checkbox5"></label></div>
+	</div>
+	<p>Send the new client an email about their account</p>
+</div>
 								</form>
 							</div>
 							{/*<!--First form End-->*/}
@@ -159,6 +166,8 @@ class RepClients_add extends React.Component {
 						{/*<!--Add product List Start-->*/}
 	
 						<div className="list-add-product">
+
+							{/*<!--List box Start-->*/}	
 							<div className="list-box">
 								<div className="top d-flex flex-wrap">
 									<div className="checkbox-cust"><input type="checkbox" id="checkbox1" />
@@ -196,7 +205,7 @@ class RepClients_add extends React.Component {
 						<div className="btn-block">
 <div class="upload-btn-wrapper">
 											<input type="file" name="Upload Document" />
-											<button class="btn common-btn-blue">
+											<button className="btn common-btn-blue">
 												<span>Upload Document</span></button>
 										</div>
 						</div>
@@ -205,6 +214,155 @@ class RepClients_add extends React.Component {
 									</div>
 								</div>
 							</div>
+							{/*<!--List box End-->*/}
+
+							{/*<!--List box Start-->*/}	
+							<div className="list-box">
+								<div className="top d-flex flex-wrap">
+									<div className="checkbox-cust"><input type="checkbox" id="checkbox2" />
+									<label for="checkbox2"></label></div>
+									<div className="title">
+										<h4>Downstream Defender</h4>	
+										<h5>Stormwater management</h5>	
+									</div>	
+									<a href="javascript:void(0);">When product was released: 02/02/2020</a>
+								</div>
+								<div className="bottom-details d-flex flex-wrap">
+									<div className="left-prod-img" style={{backgroundImage: `url(${hydroImage})`}}>
+										
+									</div>
+
+									<div className="right-prod-upload">
+									<form>
+									<div className="form-group">
+									 <label>Seller</label>
+  									 <input type="text" name="seller" />
+									</div>
+<div className="form-group">
+									 <label>Purchase date</label>
+  									 <input type="text" name="purchase-date" />
+									</div>
+<div className="form-group">
+									 <label>Cost</label>
+  									 <input type="text" name="cost" />
+									</div>
+<div className="form-group">
+									 <label>Item ID</label>
+  									 <input type="text" name="item-id" />
+									</div>
+
+						<div className="btn-block">
+<div class="upload-btn-wrapper">
+											<input type="file" name="Upload Document" />
+											<button className="btn common-btn-blue">
+												<span>Upload Document</span></button>
+										</div>
+						</div>
+
+</form>
+									</div>
+								</div>
+							</div>
+							{/*<!--List box End-->*/}	
+
+							{/*<!--List box Start-->*/}	
+							<div className="list-box">
+								<div className="top d-flex flex-wrap">
+									<div className="checkbox-cust"><input type="checkbox" id="checkbox2" />
+									<label for="checkbox2"></label></div>
+									<div className="title">
+										<h4>Hydro Biofilter</h4>	
+										<h5>Stormwater management</h5>	
+									</div>	
+									<a href="javascript:void(0);">When product was released: 02/02/2020</a>
+								</div>
+								<div className="bottom-details d-flex flex-wrap">
+									<div className="left-prod-img" style={{backgroundImage: `url(${hydroImage})`}}>
+										
+									</div>
+
+									<div className="right-prod-upload">
+									<form>
+									<div className="form-group">
+									 <label>Seller</label>
+  									 <input type="text" name="seller" />
+									</div>
+<div className="form-group">
+									 <label>Purchase date</label>
+  									 <input type="text" name="purchase-date" />
+									</div>
+<div className="form-group">
+									 <label>Cost</label>
+  									 <input type="text" name="cost" />
+									</div>
+<div className="form-group">
+									 <label>Item ID</label>
+  									 <input type="text" name="item-id" />
+									</div>
+
+						<div className="btn-block">
+<div class="upload-btn-wrapper">
+											<input type="file" name="Upload Document" />
+											<button className="btn common-btn-blue">
+												<span>Upload Document</span></button>
+										</div>
+						</div>
+
+</form>
+									</div>
+								</div>
+							</div>
+							{/*<!--List box End-->*/}	
+
+							{/*<!--List box Start-->*/}	
+							<div className="list-box">
+								<div className="top d-flex flex-wrap">
+									<div className="checkbox-cust"><input type="checkbox" id="checkbox1" />
+									<label for="checkbox1"></label></div>
+									<div className="title">
+										<h4>First Defense</h4>	
+										<h5>Stormwater management</h5>	
+									</div>	
+									<a href="javascript:void(0);">When product was released: 02/02/2020</a>
+								</div>
+								<div className="bottom-details d-flex flex-wrap">
+									<div className="left-prod-img" style={{backgroundImage: `url(${hydroImage})`}}>
+										
+									</div>
+
+									<div className="right-prod-upload">
+									<form>
+									<div className="form-group">
+									 <label>Seller</label>
+  									 <input type="text" name="seller" />
+									</div>
+<div className="form-group">
+									 <label>Purchase date</label>
+  									 <input type="text" name="purchase-date" />
+									</div>
+<div className="form-group">
+									 <label>Cost</label>
+  									 <input type="text" name="cost" />
+									</div>
+<div className="form-group">
+									 <label>Item ID</label>
+  									 <input type="text" name="item-id" />
+									</div>
+
+						<div className="btn-block">
+<div class="upload-btn-wrapper">
+											<input type="file" name="Upload Document" />
+											<button className="btn common-btn-blue">
+												<span>Upload Document</span></button>
+										</div>
+						</div>
+
+</form>
+									</div>
+								</div>
+							</div>
+							{/*<!--List box End-->*/}
+
 						</div>	
 	
 						{/*<!--Add product List End-->*/}
@@ -215,6 +373,64 @@ class RepClients_add extends React.Component {
 							{/*<!--Container End-->*/}
 							</div>
 							{/*<!--Add Product client End-->*/}
+
+							{/*<!--Add contract client Start-->*/}
+							<div className="add-contract">
+								{/*<!--Add Container Start-->*/}
+								<div className="container">
+								{/*<!--Add product Title Start-->*/}
+								<div className="pro-title d-flex flex-wrap align-center">
+									<div className="name-of-heading d-flex flex-wrap align-center">
+										<img src={require("../../images/your-product-blue-logo.svg")} alt="product-logo"/>
+										<h3>Add contract</h3>
+									</div>
+								</div>
+								{/*<!--Add product Title End-->*/}
+
+								{/*<!--Add contract form Start-->*/}
+								<div className="form-contracts">
+								<form>
+									<div className="form-group">
+									 <label>Title</label>
+  									 <input type="text" name="Title" />
+									</div>
+<div className="form-group">
+									 <label>Description</label>
+  									 <input type="text" name="description" />
+									</div>
+<div className="form-group">
+									 <label>Product tags</label>
+  									 <input type="text" name="product-tags" />
+									</div>
+<div className="form-group">
+									 <label>Sharepoint URL</label>
+  									 <input type="text" name="sharepoint-url" />
+									</div>
+
+						<div className="btn-block">
+<div class="upload-btn-wrapper">
+											<input type="file" name="Upload Document" />
+											<button className="btn common-btn-blue">
+												<span>Upload Document</span></button>
+										</div>
+						</div>
+
+</form>
+								</div>
+								{/*<!--Add contract form Endd-->*/}
+
+								</div>
+								{/*<!--Add Container End-->*/}
+							</div>
+							{/*<!--Add contract client End-->*/}
+
+							<div className="btn-block add-client">
+								<div className="upload-btn-wrapper">
+											<input type="file" name="Add new client" />
+											<button class="btn common-btn-blue">
+												<span>Add new client</span></button>
+										</div>
+							</div>
 	
 						</div>
 					{/*<!--Top Add client block Start-->*/}

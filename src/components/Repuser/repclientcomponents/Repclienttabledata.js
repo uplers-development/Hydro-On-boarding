40c824e59bbf740f2e7a201e7f75f12192589c5c
Janardan Bhatt <projects@webbymonks.com>
2020-07-13 23:04:08 +0530

    changes with files

--- src/components/Repuser/repclientcomponents/Repclienttabledata.js
+++ src/components/Repuser/repclientcomponents/Repclienttabledata.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import Apiurl,{base_url,site_url} from '../../Apiurl'; 
 
-const  Repclienttabledata=(props)=>{
+class Repclienttabledata extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			parentchecked:false,
+
+		}
+	this.handleChecked=this.handleChecked.bind(this);
+	this.handleChecked=this.handleChecked.bind(this);
+	}
+
+	handleChecked=()=>{
+		this.setState({parentchecked:true});
+	}
+
+	/*singleChecked=(e)=>{
+		console.log(e.target.getAttribute("checked")!==null ? true :false)
+	}*/
+
+
+
+	render(){
 		return (
 			<div className="clients-table table-outer">
 						   <div className="table-responsive">
@@ -10,8 +31,8 @@ const  Repclienttabledata=(props)=>{
 							            <tr>
 							               <th>
 							                  <div className="checkbox-cust">
-							                     <input type="checkbox" id="checkbox" name="checkbox[]" />
-							                     <label htmlFor="checkbox1"></label>	 
+							                     <input type="checkbox" id="checkbox" name="checkbox" onChange={this.handleChecked} />
+							                     <label htmlFor="checkbox"></label>	 
 							                  </div>
 							                  <span>Name</span>
 							               </th>
@@ -21,11 +42,11 @@ const  Repclienttabledata=(props)=>{
 							            </tr>
 							         </thead>
 							          <tbody>
-						             {props.clientdataTable.map((item,index)=>
+						             {this.props.clientdataTable.map((item,index)=>
 							            <tr key={index}>
 							               <td>
 							                  <div className="checkbox-cust">
-							                     <input type="checkbox" id={"checkbox"+index} name="checkbox" />
+							                     <input type="checkbox" id={"checkbox"+index} className="clientchecked" checked={this.state.parentchecked ? true : false} name="checkbox" onChange={this.singleChecked} />
 							                     <label htmlFor={"checkbox"+index}></label>	 
 							                  </div>
 							                  <div className="name-edit">
@@ -52,6 +73,7 @@ const  Repclienttabledata=(props)=>{
 						   </div>
 			</div>
 		);
+	}
 }
 
 export default Repclienttabledata;

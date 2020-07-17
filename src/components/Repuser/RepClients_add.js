e177c7e146e46178048dc5d22653451fa3a9ef09
uplers-development <projects@webbymonks.com>
2020-07-17 16:25:35 +0530

    chanes iwth files

--- src/components/Repuser/RepClients_add.js
+++ src/components/Repuser/RepClients_add.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, Redirect } from "react-router-dom";
 import CommonBackground from '../../images/common-bg.jpg';
+import{hasNull,isRequired,hasValidEmail,hasValidMobile,hasValidPassword} from '../validation';
 import Sidebar from '../assets/Sidebar';
 import UserProfile from '../assets/UserProfile';
 import Apiurl,{site_url,base_url} from '../Apiurl'; 
@@ -11,6 +12,8 @@ import hydroImage from '../../images/hydro-biofilter-product.jpg';
 import Repaddclient from './repclientcomponents/Repaddclient'
 import Repaddproduct from './repclientcomponents/Repaddproduct'
 import Repaddcontract from './repclientcomponents/Repaddcontract'
+import {ValidationMsg} from'../constants/validationmsg';
+
 let object={};
 let productList=[];
 class RepClients_add extends React.Component {
@@ -23,6 +26,13 @@ class RepClients_add extends React.Component {
       getProductadd:null,
       getContractadd:null,
       openPopup:false,
+      firstname:false,
+      company:false,
+      surname:false,
+      email:false,
+      role:false,
+      contact:false,
+      password:false,
 			fromProductSec:this.props.location.state!==undefined ? this.props.location.state.productPage :'',
 			fromContractSec:this.props.location.state!==undefined ? this.props.location.state.contractPage : '',
       sectionCalldiversion:null,
@@ -50,6 +60,8 @@ class RepClients_add extends React.Component {
       }
       
    }
+
+ 
 
    get_product_to_add=(get_product_value)=>{
     console.log(get_product_value);
@@ -103,6 +115,9 @@ class RepClients_add extends React.Component {
 
   submitClientDetails=(e)=>{
     e.preventDefault();
+    var e = document.getElementById("time_zone");
+    var strUser = e.options[e.selectedIndex].value;
+    if(hasValidEmail(document.querySelector("#email").value) && hasValidMobile(document.querySelector("#contact").value)){
     let option={  
            "field_first_name" : [{"value":document.getElementById("fname") && document.querySelector("#fname").value!=='' ? document.querySelector("#fname").value : ''}],
            "field_last_name" : [{"value":document.getElementById("sname") && document.querySelector("#sname").value!=='' ? document.querySelector("#sname").value :''}],
@@ -110,7 +125,7 @@ class RepClients_add extends React.Component {
            "field_organisation" : [{"value":document.getElementById("company") && document.querySelector("#company").value!=='' ? document.querySelector("#company").value :''}],
            "field_job_title" : [{"value":document.getElementById("role") && document.querySelector("#role").value!=='' ? document.querySelector("#role").value :''}],
            "field_contact_number" : [{"value":document.getElementById("contact") && document.querySelector("#contact").value!=='' ? document.querySelector("#contact").value :''}],
-           "timezone" : [{"value":"UTC"}],
+           "timezone" : [{"value":strUser}],
            "name" : [{"value":document.getElementById("email") && document.querySelector("#email").value!=='' ? document.querySelector("#email").value :''}],
            "pass" : [{"value":document.getElementById("password") && document.querySelector("#password").value!=='' ? document.querySelector("#password").value :''}],
            "roles" : [{ "target_id":"client" }],
@@ -205,6 +220,31 @@ class RepClients_add extends React.Component {
       }catch(err){
          console.log(err);
       }
+  }else{
+      
+        /*if(hasNull(document.querySelector("#fname").value)){
+          document.querySelector(".form-group #fname").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid firstname.</span>')
+        }
+        if(hasNull(document.querySelector("#sname").value)){
+          document.querySelector(".form-group #sname").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid surname.</span>')
+        }
+        if(!hasValidEmail(document.querySelector("#email").value)){
+          document.querySelector(".form-group #email").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid email.</span>')
+        } if(hasNull(document.querySelector("#company").value)){
+          document.querySelector(".form-group #company").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid company name.</span>')
+        }if(hasNull(document.querySelector("#role").value)){
+          document.querySelector(".form-group #role").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid role.</span>')
+        }if(!hasValidMobile(document.querySelector("#contact").value)){
+          document.querySelector(".form-group #contact").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid contactnumber.</span>')
+        }
+        if(!hasValidPassword(document.querySelector("#password").value)){
+          document.querySelector(".form-group #password").insertAdjacentHTML("afterend",'<span class="empty-field">Please enter your valid password.</span>')
+        }*/
+
+
+
+        console.log(this.state.firstname);
+    }
   }
 
 
@@ -236,7 +276,8 @@ class RepClients_add extends React.Component {
                             <h4>Create a brand new client user and add them to this site</h4> 
                         </div>
                         <div className="clients-add">
-                           <Repaddclient getClienttoadd={this.get_client_to_be_add}/>
+                           <Repaddclient getClienttoadd={this.get_client_to_be_add} firstname={
+                            this.state.firstname} company={this.state.company} lastname={this.state.surname} email={this.state.email} role={this.state.role} contact={this.state.contact} password={this.state.password} />
                            <Repaddproduct getproducttoadd={this.get_product_to_add}/>
                            <Repaddcontract getcontracttoadd={this.get_contract_to_be_add}/>
                         </div>

7e9d0148f7971da4e309db8525cbfd774f13b232
uplers-development <projects@webbymonks.com>
2020-07-28 23:36:02 +0530

    changs wth product section

--- src/components/Admin/admin-products.js
+++ src/components/Admin/admin-products.js
@@ -7,6 +7,7 @@ import Adminnavbar from './assets/Adminnavbar'
 import Adminheader from './assets/Adminheader'
 import Adminproductfilter from './Admincomponents/Adminproductfilter'
 import Adminproductmobilefilter from './Admincomponents/Adminproductmobilefilter'
+import Adminproducttable from './Admincomponents/Adminproducttable'
 import adminProductImage from '../../images/headcell2x.png';
 import adminProductImage2 from '../../images/hydro-gritcleanse2x.png';
 
@@ -66,83 +67,7 @@ class AdminProduct extends React.Component {
 							</div>
 						
 					</div>
-					{/*<!--Top filter block End-->*/}
-
-
-					{/*<!--Client block Start-->*/}
-						<div className="admin-products-table table-outer">
-							<div className="table-responsive">
-							
-								{/*<!--Table Start-->*/}
-								<table className="table table-striped striped-gray">
-   <thead>
-      <tr>
-         <th>
-		 <div className="checkbox-cust">
-				 	<input type="checkbox" id="checkbox0" />
-      				<label htmlfor="checkbox0"></label>	 
-			 </div><span>Product</span>
-		</th>
-         <th>Author</th>
-         <th>Data</th>
-         <th>Type</th>
-         <th>Last Modified</th>
-      </tr>
-   </thead>
-   <tbody>
-      <tr>
-         <td>
-			 <div className="checkbox-cust">
-				 	<input type="checkbox" id="checkbox1" />
-      				<label htmlfor="checkbox1"></label>	 
-			 </div>
-			 <div className="name-edit">
-				 <div className="img-c bg-cover" style={{backgroundImage: `url(${adminProductImage})`}}></div>
-				 <div className="right-detail">
-				 <h3>Lorem ipsum dolor</h3><a href="#" title="Draft">Draft</a>
-			     <div className="action d-flex flex-wrap">
-					<a href="#" title="Edit">Edit</a>	 
-					<a href="#" title="Delete">Delete</a>	 
-					<a href="#" title="View">View</a>	 
-				 </div>	
-				</div>	
-			 </div>	 
-		 </td>
-		 <td>Simon</td>				
-		 <td><span>Last Modified</span><span>3 hours ago</span></td>				
-		 <td>Stormwater Management</td>				
-		 <td>18th March 2020</td>				
-      </tr>
-
-<tr>
-         <td>
-			 <div className="checkbox-cust">
-				 	<input type="checkbox" id="checkbox2" />
-      				<label htmlfor="checkbox2"></label>	 
-			 </div>
-			 <div className="name-edit">
-				 <div className="img-c bg-cover" style={{backgroundImage: `url(${adminProductImage2})`}}></div>
-				 <div className="right-detail">
-				 <h3>Lorem ipsum dolor</h3><a href="#" title="Draft">Draft</a>
-			 	</div>	
-			 </div>	 
-		 </td>
-		 <td>Simon</td>				
-		 <td><span>Last Modified</span><span>3 hours ago</span></td>				
-		 <td>Stormwater Management</td>				
-		 <td>18th March 2020</td>				
-      </tr>
-	
-	
-         </tbody>
-</table>
-								{/*<!--Table End-->*/}
-								
-							</div>	
-						</div>
-					{/*<!--Top filter block Start-->*/}
-						
-						
+						<Adminproducttable/>
 					</div>
 					{/*<!--Resource main blok end-->*/}
 

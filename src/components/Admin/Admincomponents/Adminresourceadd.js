8a01ae58a32cc928fbe348056ce93d859cd6a096
webuplers <web@uplers.com>
2020-07-28 20:11:55 +0530

    css for add resource

--- src/components/Admin/Admincomponents/Adminresourceadd.js
+++ src/components/Admin/Admincomponents/Adminresourceadd.js
@@ -283,13 +283,15 @@ OnSubmitResource=(e)=>{
 								{this.state.imageFormateState ? ValidationMsg.common.default.imageformate : ''}
 								</div>
 					            
+								
+								<div className="thumbnail-upload-right">
 					            {this.state.smallLoader ? 
 										<div className="loader"></div>
 									:
 				            	<div className="upload-thumbnail-img bg-cover" style={{backgroundImage: `url(${this.state.uploadedresourceimage!=='' ? this.state.uploadedresourceimage : ThumbnailImage})`}}>
-									<div className="loader"></div>
+									
 								</div>
-				            }
+				            }</div>
 				   		</div>
 					   <div className="btn-block">
 						   <button className="btn wide common-btn-blue">

5f1e396abb53ba9991dd1b5526ebfc08c5f5ab6e
uplers-development <projects@webbymonks.com>
2020-08-27 14:26:03 +0530

    changes with fiels

--- src/components/Repuser/repclientcomponents/Repclientbulkaction.js
+++ src/components/Repuser/repclientcomponents/Repclientbulkaction.js
@@ -105,6 +105,9 @@ render(){
 				<div className="btn-block mobile-hide">
 				   <button className="common-btn-blue" onClick={(e)=>this.setState({openPopup:true})}><span>APPLY</span></button>
 				</div>
+				<div className="btn-block mobile-show add-client">
+    				<Link to={"/RepClients_add"} className="common-btn-blue"><span>Add Client</span></Link>
+    			</div>
 				{this.state.openPopup ? 
 					<div id="modal" className="modal-container">
 						<div className="modal d-flex flex-wrap align-center justify-center">
